b5dd0269660c4737a6c9c9d4c597d0f5f903bfb2
asthik <astx.kras@gmail.com>
2017-11-03 12:11:39 +0400

    html

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*function showMessage(){
+	var name = prompt("What is Yor name: ");
+	alert("Hello " + name);
+}
+showMessage(); 
+
+function showName(){
+	var name = document.getElementById("login");
+	alert(name + " hello");
+}
+showName();*/
+alert(location.href);
+console.log(document.body); 
